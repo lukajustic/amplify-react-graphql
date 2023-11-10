2e03fdd28c1c396ad335848fc92683eb00b0fe3d
Luka Justic <lukjus@amazon.com>
2023-11-10 18:00:41 +0000

    Edit header

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText("Hello from V2");
+  expect(headerElement).toBeInTheDocument();
 });
